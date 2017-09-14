c0af05ebb95b6d86b0385f12250391860c8e0b97
thomasblommaert <thomas_blommaert@hotmail.com>
2017-09-14 14:59:09 +0200

    Disable Angular CLI warning for exported interface

--- src/country-picker.config.ts
+++ src/country-picker.config.ts
@@ -2,8 +2,8 @@ import { OpaqueToken } from '@angular/core';
 
 export let COUNTRY_PICKER_CONFIG = new OpaqueToken('countrypicker.config');
 
-export interface CountryPickerConfig {
-    baseUrl: string,
+export class CountryPickerConfig {
+    baseUrl: string
 }
 
 export const COUNTRY_PICKER_CONFIG_DEFAULT: CountryPickerConfig = {
